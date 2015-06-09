33ed62747563ecfe1609be10b7697908ee0c501c
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-06-09 15:06:04 +0200

    minor

--- tests/TestHelperTrait.php
+++ tests/TestHelperTrait.php
@@ -9,14 +9,19 @@ trait TestHelperTrait
 {
     private $psr4namespace = "Nixshell\\";
 
+    /**
+     * @param string $class
+     * @return string
+     */
     public function getPsr4FullName($class)
     {
         return $this->psr4namespace . $class;
     }
 
     /**
-     * @param $command
-     * @return \PHPUnit_Framework_MockObject_MockObject|ExecutorInterface
+     * @param string $command
+     * @param bool $enabled
+     * @return ExecutorInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     protected function getExecutorBaseStub($command, $enabled = true)
     {
